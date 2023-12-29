b59fef76da516453682b493f09f4102c0c26b133
Amangoua Ange <adagou@yahoo.com>
2023-12-29 09:13:19 +0000

    remove grid on hero

--- components/home/hero/Hero.tsx
+++ components/home/hero/Hero.tsx
@@ -2,7 +2,6 @@ import Image from "next/image";
 import { motion } from "framer-motion";
 import { StandardButton } from "@/components/buttons/StandardButton";
 import { Reveal } from "@/components/utils/Reveal";
-import { DotGrid } from "./DotGrid";
 import styles from "./hero.module.scss";
 
 export const Hero = () => {
@@ -30,7 +29,7 @@ export const Hero = () => {
             </h1>
           </Reveal>
           <Reveal>
-            <h2 className={styles.subTitle}>Ange-thierry.</h2>
+            <h2 className={styles.subTitle}>Ange-Thierry.</h2>
           </Reveal>
         </div>
         <motion.div
@@ -39,7 +38,6 @@ export const Hero = () => {
           transition={{ duration: 0.5, delay: 0.4 }}
         ></motion.div>
       </div>
-      <DotGrid />
     </section>
   );
 };
